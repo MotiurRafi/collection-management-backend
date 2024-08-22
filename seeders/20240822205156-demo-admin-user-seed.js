9d8f30c67b80dc946622300b30e64d45d037c915
MotiurRafi <motiurrafi601@gmail.com>
2024-08-23 02:53:31 +0600

    finish23

---
+++ seeders/20240822205156-demo-admin-user-seed.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const hashedPasswordAdmin = await bcrypt.hash('01305441850', 10);
+    const hashedPasswordUser = await bcrypt.hash('01305441850', 10);
+
+    await queryInterface.bulkInsert('Users', [
+      {
+        username: 'Motiur Rahman Rafi',
+        email: 'motiurrafi601@gmail.com',
+        password: hashedPasswordAdmin,
+        role: 'admin',
+        status: 'active',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'demoUser1',
+        email: 'mr@gmail.com',
+        password: hashedPasswordUser,
+        role: 'user',
+        status: 'active',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'demoUser2',
+        email: 'demo2@example.com',
+        password: hashedPasswordUser,
+        role: 'user',
+        status: 'active',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', {
+      username: ['Motiur Rahman Rafi', 'demoUser1', 'demoUser2']
+    }, {});
+  }
+};
