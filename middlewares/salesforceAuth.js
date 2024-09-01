9196336082e1265890f6d20022d79cca84eca4c8
MotiurRafi <motiurrafi601@gmail.com>
2024-09-02 02:30:42 +0600

    token

--- middlewares/salesforceAuth.js
+++ middlewares/salesforceAuth.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 exports.getSalesforceToken = async (req, res) => {
   const { codeVerifier, code: authCode } = req.body;
-  try {
+  console.log('Received codeVerifier:', codeVerifier);
+  console.log('Received authCode:', authCode);
+    try {
     const tokenResponse = await axios.post(`${process.env.SALESFORCE_LOGIN_URL}/services/oauth2/token`, {
       grant_type: 'authorization_code',
       client_id: process.env.SALESFORCE_CONSUMER_KEY,
@@ -12,9 +14,11 @@ exports.getSalesforceToken = async (req, res) => {
       code: authCode,
       code_verifier: codeVerifier
     });
+    console.log("tokenResponse : ", tokenResponse.data)
     const { access_token, instance_url } = tokenResponse.data;
     res.status(200).json({ accessToken: access_token, instanceUrl: instance_url });
   } catch (error) {
+    console.error('Error exchanging code for token:', error.response ? error.response.data : error.message);
     res.status(500).send('Failed to exchange code for token');
   }
 };
