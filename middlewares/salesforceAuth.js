6a6c482d1b7dc894062ab4ad7c4a50c53eaa4735
MotiurRafi <motiurrafi601@gmail.com>
2024-09-02 02:53:10 +0600

    content-type

--- middlewares/salesforceAuth.js
+++ middlewares/salesforceAuth.js
@@ -1,12 +1,13 @@
 const axios = require('axios');
+const qs = require('qs');
 require('dotenv').config();
 
 exports.getSalesforceToken = async (req, res) => {
   const { codeVerifier, code: authCode } = req.body;
-  console.log('Received codeVerifier:', codeVerifier);
-  console.log('Received authCode:', authCode);
-    try {
-    const tokenResponse = await axios.post(`${process.env.SALESFORCE_LOGIN_URL}/services/oauth2/token`, {
+  console.log(codeVerifier, authCode);
+  
+  try {
+    const data = qs.stringify({
       grant_type: 'authorization_code',
       client_id: process.env.SALESFORCE_CONSUMER_KEY,
       client_secret: process.env.SALESFORCE_CONSUMER_SECRET,
@@ -14,12 +15,18 @@ exports.getSalesforceToken = async (req, res) => {
       code: authCode,
       code_verifier: codeVerifier
     });
-    console.log("tokenResponse : ", tokenResponse.data)
+
+    const tokenResponse = await axios.post(`${process.env.SALESFORCE_LOGIN_URL}/services/oauth2/token`, data, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+
     const { access_token, instance_url } = tokenResponse.data;
     res.status(200).json({ accessToken: access_token, instanceUrl: instance_url });
   } catch (error) {
     console.error('Error exchanging code for token:', error.response ? error.response.data : error.message);
-    res.status(500).send('Failed to exchange code for token');
+    res.status(500).json({ message: 'Failed to exchange code for token', error: error.response ? error.response.data : error.message });
   }
 };
 
